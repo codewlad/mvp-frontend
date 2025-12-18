88f0493933102e8d69e5cac9ce2f06e93f4f07ec
codewlad <codewlad@gmail.com>
2025-12-18 11:09:51 -0300

    added user route

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,34 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [users, setUsers] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch(import.meta.env.VITE_API_URL + "/users")
-      .then(res => res.json())
-      .then(setData);
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) return <p>Carregando...</p>;
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Fullstack funcionando 🚀</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Usuários do banco 🚀</h1>
+
+      {users.length === 0 && <p>Nenhum usuário encontrado</p>}
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> — {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
